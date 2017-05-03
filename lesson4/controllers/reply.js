ff7c3b3b25dd823716ab735797747e89b0f8b5ad
xianyulaodi <1009140297@qq.com>
2017-05-03 23:20:16 +0800

    新增controller

---
+++ lesson4/controllers/reply.js
@@ -0,0 +1,44 @@
+const validator = require('validator');
+const _ = require('lodash');
+const Eventproxy = require('eventproxy');
+const Reply = require('../api/reply');
+
+exports.add = (req,res,next) => {
+    const topicId = req.body.topicId;
+    const content = req.body.content;
+    const topicNode = req.body.topicNode;
+    const topicTitle = req.body.topicTitle;
+    const con = validator.trim(content);
+    const replyerId = req.session.user._id;
+    const replyer_profile = req.session.user.profile_image_url;
+    const replyerName = req.session.user.loginname;
+    const attached ={
+      topicTitle:topicTitle,
+      topicNode:topicNode,
+      topicId:topicId,
+      replyerId:replyerId,
+      replyerName:replyerName,
+      replyer_profile:replyer_profile
+    }
+    const proxy = new Eventproxy();
+    var data={
+      content:con,
+      replyer_id:replyerId,
+      topic_id:topicId,
+      topic_title:topicTitle,
+      replyer_profile:replyer_profile,
+      replyer_name:replyerName
+    }
+    Reply.newAndSave(data,(err) => {
+      if(err) return;
+      proxy.emit('reply');
+    });
+    Reply.updateLastReply(topicId,replyerId,replyerName,(err) => {
+      if(err) return;
+      proxy.emit('topic',topicId);
+    });
+    proxy.all('topic','reply',(topicId) => {
+      return res.redirect('/topic?id='+topicId+'&node_cat='+topicNode);
+    });
+
+}
